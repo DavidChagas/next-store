96d3b3655b7a0095b261180a53175cd6168b5035
DavidChagas <davidc@windowslive.com>
2023-06-06 13:35:05 -0300

    prettier tailwind

--- src/app/components/produto/BotaoComprar.tsx
+++ src/app/components/produto/BotaoComprar.tsx
@@ -8,7 +8,14 @@ export default function BotaoComprar() {
   return (
     <button
       type="button"
-      className="mt-1 p-1 w-[100%] text-center text-green-700 border border-green-700 rounded-md font-semibold hover:bg-green-700 hover:text-white transition duration-150 ease-out hover:ease-in"
+      className="
+        mt-1 w-[100%] 
+        rounded-md border border-green-700  
+        p-1 
+        text-center font-semibold text-green-700 
+        transition duration-150 ease-out 
+        hover:bg-green-700 hover:text-white hover:ease-in
+      "
       onClick={comprar}
     >
       Comprar
